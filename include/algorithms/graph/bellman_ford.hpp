cbd4e71a311941c88576de033bc378749437a25a
Ahmed AbouEleyoun <amedoeyes@gmail.com>
2024-12-28 23:10:23 +0200

    add bellman ford

---
+++ include/algorithms/graph/bellman_ford.hpp
@@ -0,0 +1,52 @@
+#ifndef DIJKSTRA_HPP
+#define DIJKSTRA_HPP
+
+#include <concepts>
+#include <optional>
+#include <ranges>
+#include <tuple>
+#include <unordered_map>
+#include <utility>
+
+template <typename G, typename V>
+	requires requires(const G& g, const V& v) {
+		requires std::ranges::range<G>;
+		{ g.at(v) } -> std::ranges::range;
+		requires std::integral<typename std::ranges::range_value_t<decltype(g.at(v))>::second_type>
+							 || std::floating_point<typename std::ranges::range_value_t<decltype(g.at(v))>::second_type>;
+		requires std::same_as<
+			std::ranges::range_value_t<decltype(g.at(v))>,
+			std::pair<V, typename std::ranges::range_value_t<decltype(g.at(v))>::second_type>>;
+	}
+constexpr auto bellman_ford(const G& graph, const V& source) -> std::optional<std::tuple<
+	std::unordered_map<V, typename std::ranges::range_value_t<decltype(graph.at(source))>::second_type>,
+	std::unordered_map<V, V>>> {
+	using weight_type =
+		typename std::ranges::range_value_t<typename std::ranges::range_value_t<G>::second_type>::second_type;
+	auto dist = std::unordered_map<V, weight_type>();
+	auto parent = std::unordered_map<V, V>();
+	dist[source] = weight_type{};
+	parent[source] = source;
+	for (auto _ : std::views::iota(0ul, graph.size() - 1)) {
+		for (const auto& [u, edges] : graph) {
+			if (!dist.contains(u)) continue;
+			const auto d = dist[u];
+			for (const auto& [v, w] : edges) {
+				if (!dist.contains(v) || d + w < dist[v]) {
+					dist[v] = d + w;
+					parent[v] = u;
+				}
+			}
+		}
+	}
+	for (const auto& [u, edges] : graph) {
+		if (!dist.contains(u)) continue;
+		const auto d = dist[u];
+		for (const auto& [v, w] : edges) {
+			if (d + w < dist[v]) return std::nullopt;
+		}
+	}
+	return std::tuple{dist, parent};
+}
+
+#endif // !DIJKSTRA_HPP
---
+++ tests/algorithms/graph/bellman_ford.cpp
@@ -0,0 +1,44 @@
+#include "algorithms/graph/bellman_ford.hpp"
+
+#include <cstdint>
+#include <print>
+#include <unordered_map>
+#include <utility>
+#include <vector>
+
+auto main() -> int {
+	const auto graph = std::unordered_map<int32_t, std::vector<std::pair<int32_t, int32_t>>>{
+		{0, {{1, 5}}},
+		{1, {{2, 1}, {3, 2}}},
+		{2, {{4, 1}}},
+		{3, {}},
+		{4, {{3, -1}}},
+	};
+	const auto cycle_graph = std::unordered_map<int32_t, std::vector<std::pair<int32_t, int32_t>>>{
+		{0, {{1, 4}}},
+		{1, {{2, -6}}},
+		{2, {{3, 5}}},
+		{3, {{1, -2}}},
+	};
+
+	const auto source = 0;
+	auto result = bellman_ford(graph, source);
+
+	if (!result.has_value()) {
+		std::println("graph has negative cycle");
+	} else {
+		auto [d, p] = *result;
+
+		std::println("Distances from node {}:", source);
+		for (const auto& [node, distance] : d) {
+			std::println("Node {}: {}", node, distance);
+		}
+
+		std::println();
+
+		std::println("Parents:");
+		for (const auto& [node, parent] : p) {
+			std::println("Node {}: {}", node, parent);
+		}
+	}
+}
