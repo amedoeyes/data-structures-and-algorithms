cc711e920aa68088bb04f1a581c0ea36ce463ebb
Ahmed AbouEleyoun <amedoeyes@gmail.com>
2024-12-28 23:10:13 +0200

    add dijkstra

---
+++ include/algorithms/graph/dijkstra.hpp
@@ -0,0 +1,52 @@
+#ifndef DIJKSTRA_HPP
+#define DIJKSTRA_HPP
+
+#include <concepts>
+#include <functional>
+#include <queue>
+#include <ranges>
+#include <set>
+#include <tuple>
+#include <unordered_map>
+#include <utility>
+#include <vector>
+
+template <typename G, typename V>
+	requires requires(const G& g, const V& v) {
+		requires std::ranges::range<G>;
+		{ g.at(v) } -> std::ranges::range;
+		requires std::integral<typename std::ranges::range_value_t<decltype(g.at(v))>::second_type>
+							 || std::floating_point<typename std::ranges::range_value_t<decltype(g.at(v))>::second_type>;
+		requires std::same_as<
+			std::ranges::range_value_t<decltype(g.at(v))>,
+			std::pair<V, typename std::ranges::range_value_t<decltype(g.at(v))>::second_type>>;
+	}
+constexpr auto dijkstra(const G& graph, const V& source) -> std::tuple<
+	std::unordered_map<V, typename std::ranges::range_value_t<decltype(graph.at(source))>::second_type>,
+	std::unordered_map<V, V>> {
+	using weight_type =
+		typename std::ranges::range_value_t<typename std::ranges::range_value_t<G>::second_type>::second_type;
+	auto dist = std::unordered_map<V, weight_type>();
+	auto parent = std::unordered_map<V, V>();
+	auto visited = std::set<V>();
+	auto pq = std::priority_queue<std::pair<weight_type, V>, std::vector<std::pair<weight_type, V>>, std::greater<>>();
+	dist[source] = weight_type{};
+	parent[source] = source;
+	pq.emplace(weight_type{}, source);
+	while (!pq.empty()) {
+		const auto [d, u] = pq.top();
+		pq.pop();
+		if (visited.contains(u) || (dist.contains(u) && dist[u] > d)) continue;
+		visited.emplace(u);
+		for (const auto& [v, w] : graph.at(u)) {
+			if (!visited.contains(v) && (!dist.contains(v) || d + w < dist[v])) {
+				dist[v] = d + w;
+				parent[v] = u;
+				pq.emplace(d + w, v);
+			}
+		}
+	}
+	return {dist, parent};
+}
+
+#endif // !DIJKSTRA_HPP
---
+++ tests/algorithms/graph/dijkstra.cpp
@@ -0,0 +1,36 @@
+#include "algorithms/graph/dijkstra.hpp"
+
+#include <cstdint>
+#include <print>
+#include <unordered_map>
+#include <utility>
+#include <vector>
+
+auto main() -> int {
+	const auto graph = std::unordered_map<int32_t, std::vector<std::pair<int32_t, int32_t>>>{
+		{0, {{1, 4}, {7, 8}}},
+		{1, {{0, 4}, {2, 8}, {7, 11}}},
+		{2, {{1, 8}, {3, 7}, {8, 2}, {5, 4}}},
+		{3, {{2, 7}, {4, 9}, {5, 14}}},
+		{4, {{3, 9}, {5, 10}}},
+		{5, {{2, 4}, {3, 14}, {4, 10}, {6, 2}}},
+		{6, {{5, 2}, {7, 1}, {8, 6}}},
+		{7, {{0, 8}, {1, 11}, {6, 1}, {8, 7}}},
+		{8, {{2, 2}, {6, 6}, {7, 7}}},
+	};
+
+	const auto source = 0;
+	auto [d, p] = dijkstra(graph, source);
+
+	std::println("Distances from node {}:", source);
+	for (const auto& [node, distance] : d) {
+		std::println("Node {}: {}", node, distance);
+	}
+
+	std::println();
+
+	std::println("Parents:");
+	for (const auto& [node, parent] : p) {
+		std::println("Node {}: {}", node, parent);
+	}
+}
